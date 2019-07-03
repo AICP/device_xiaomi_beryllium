7ecc16c0e22881df5549e80e769945cb5964d124
Michele Bono <bono.michele94@gmail.com>
2019-07-03 10:10:40 +0200

    beryllium: add netflix custom property

--- device-props.mk
+++ device-props.mk
@@ -5,3 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
+
+#Netflix custom property
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q845-05000-1
