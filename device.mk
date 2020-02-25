0b4a018e2a13d7b5bc86327bb02d4f17ef5f7eb2
Michele Bono <bono.michele94@gmail.com>
2020-02-25 10:01:05 +0100

    beryllium: Initial AICP bringup

--- device.mk
+++ device.mk
@@ -22,13 +22,10 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
